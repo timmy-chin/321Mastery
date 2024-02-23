8cdab31f10832e0019fe4e5c348fefdd2c707384
Ivan Nghi <inghi@calpoly.edu>
2024-02-22 19:50:05 -0800

    mvp data model

--- prisma/migrations/20240223032654_mvp_data_model/migration.sql
+++ prisma/migrations/20240223032654_mvp_data_model/migration.sql
@@ -6,8 +6,8 @@ CREATE TABLE "User" (
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
     "gender" TEXT NOT NULL,
-    "cpVerified" INTEGER NOT NULL,
-    "dlVerified" INTEGER NOT NULL,
+    -- "cpVerified" BOOLEAN NOT NULL,
+    -- "dlVerified" BOOLEAN NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -29,7 +29,10 @@ CREATE TABLE "Request" (
     "id" SERIAL NOT NULL,
     "request" BOOLEAN NOT NULL,
     "time" TIMESTAMP(3) NOT NULL,
+    "rideId" INTEGER NOT NULL,
     "requesterId" INTEGER NOT NULL,
+    "status" BOOLEAN NOT NULL,
+    "accepted" BOOLEAN NOT NULL,
 
     CONSTRAINT "Request_pkey" PRIMARY KEY ("id")
 );
@@ -45,7 +48,6 @@ CREATE TABLE "Ride" (
     "startTime" TIMESTAMP(3) NOT NULL,
     "numStops" INTEGER NOT NULL,
     "driverId" INTEGER NOT NULL,
-    "riderId" INTEGER NOT NULL,
 
     CONSTRAINT "Ride_pkey" PRIMARY KEY ("id")
 );
@@ -53,8 +55,23 @@ CREATE TABLE "Ride" (
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Message_senderId_key" ON "Message"("senderId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Request_rideId_key" ON "Request"("rideId");
+
 -- AddForeignKey
 ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Message" ADD CONSTRAINT "Message_recipientId_fkey" FOREIGN KEY ("recipientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_rideId_fkey" FOREIGN KEY ("rideId") REFERENCES "Ride"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_requesterId_fkey" FOREIGN KEY ("requesterId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Ride" ADD CONSTRAINT "Ride_driverId_fkey" FOREIGN KEY ("driverId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
