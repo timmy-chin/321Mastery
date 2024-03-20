c8a8c1ca61c2ca126066abe7bc5703334b6f8908
Timmy Chin <82336477+timmy-chin@users.noreply.github.com>
2024-03-19 22:26:06 -0700

    deleted all the prisma data

--- prisma/migrations/20240319072334_added_rating/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Rating" (
-    "id" SERIAL NOT NULL,
-    "driverId" INTEGER NOT NULL,
-    "rating" INTEGER NOT NULL,
-
-    CONSTRAINT "Rating_pkey" PRIMARY KEY ("id")
-);
